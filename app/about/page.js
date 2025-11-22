6206091ef0e1bc680bb7e97406431027626cef94
Rishikesh Negi <rishinegi97@gmail.com>
2025-11-22 16:10:19 +0530

    Create base layout and core pages

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+import Navigation from "../components/navigation";
+
+export default function Page() {
+  return (
+    <div>
+      <h3>About us</h3>;
+    </div>
+  );
+}
---
+++ app/account/page.js
@@ -0,0 +1,9 @@
+import Navigation from "../components/navigation";
+
+export default function Page() {
+  return (
+    <div>
+      <h3>Account</h3>;
+    </div>
+  );
+}
