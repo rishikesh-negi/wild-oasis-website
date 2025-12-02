b85e58785f147722ebceb8bbadce73a9771d2f99
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-02 11:16:33 +0530

    Remove the unnecessary semicolon

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,7 @@ import Navigation from "../components/navigation";
 export default function Page() {
   return (
     <div>
-      <h3>About us</h3>;
+      <h3>About us</h3>
     </div>
   );
 }
--- app/account/page.js
+++ app/account/page.js
@@ -3,7 +3,7 @@ import Navigation from "../components/navigation";
 export default function Page() {
   return (
     <div>
-      <h3>Account</h3>;
+      <h3>Account</h3>
     </div>
   );
 }
