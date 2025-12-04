f318224239a2461c7a6f9d354e940d43b88e839b
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-04 15:51:13 +0530

    Add logo, font, global styles, and metadata

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,30 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/navigation";
+import Logo from "@/app/_components/Logo";
+
+import { Josefin_Sans } from "next/font/google";
+
+import "@/app/_styles/globals.css";
+import Navigation from "./_components/navigation";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
-  title: "The Wild Oasis",
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious boutique hotel located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 h-dvh`}>
         <header>
           <Logo />
           <Navigation />
