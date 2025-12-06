ad148b2b1a5a41c1d1649d52869f16cd49cf23d6
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-06 11:48:23 +0530

    Define options for image quality configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    qualities: [
+      25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100,
+    ],
+  },
 };
 
 export default nextConfig;
