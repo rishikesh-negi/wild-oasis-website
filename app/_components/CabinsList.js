f519671c0d41900e1997bc5f02b99ebd96e70267
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-22 16:08:30 +0530

    Cache fetched cabins and filter them based on the selected filter in the URL search params

--- app/_components/CabinsList.js
+++ app/_components/CabinsList.js
@@ -1,16 +1,31 @@
-import { use } from "react";
-
 import { getCabins } from "../_lib/data-service";
 import CabinCard from "@/app/_components/CabinCard";
+import { cacheLife } from "next/cache";
 
-function CabinsList() {
-  const cabins = use(getCabins());
+async function CabinsList({ filter }) {
+  "use cache";
+  cacheLife({ revalidate: 60 * 60, expire: 60 * 90 });
+
+  const cabins = await getCabins();
 
   if (!cabins.length) return null;
 
+  const filteredCabins = cabins.filter((cabin) => {
+    switch (filter) {
+      case "small":
+        return cabin.maxCapacity <= 3;
+      case "medium":
+        return cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7;
+      case "large":
+        return cabin.maxCapacity >= 8;
+      default:
+        return true;
+    }
+  });
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {filteredCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
