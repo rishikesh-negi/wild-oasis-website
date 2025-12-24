eb61a93214d81b703beb3e64cd4919a7d66289d1
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-24 15:50:07 +0530

    Create component to remind user about unreserved selected dates

---
+++ app/_components/ReservationReminder.js
@@ -0,0 +1,31 @@
+"use client";
+
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import { format } from "date-fns";
+import { useReservation } from "../contexts/ReservationContext";
+
+function ReservationReminder() {
+  // CHANGE
+  const { range, resetRange } = useReservation();
+
+  if (!range.from || !range.to) return null;
+
+  return (
+    <div className="fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-md bg-accent-500 text-primary-800 text  font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center">
+      <p>
+        Don&apos;t forget to reserve your dates <br /> from{" "}
+        {format(new Date(range.from), "MMM dd yyyy")} to{" "}
+        {format(new Date(range.to), "MMM dd yyyy")}
+      </p>
+      <button
+        className="flex px-3 py-1 border border-primary-800 items-center rounded-md gap-1 p-1 hover:bg-accent-400 transition-all cursor-pointer"
+        onClick={resetRange}>
+        <span className="text-sm tracking-wide font-semibold">
+          Clear selection
+        </span>
+      </button>
+    </div>
+  );
+}
+
+export default ReservationReminder;
