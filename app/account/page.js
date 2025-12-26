0914d204b4986fda081875a528895343d3e799c9
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-26 16:12:31 +0530

    Dynamically display the first name of authenticated user

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,17 @@
+import { use } from "react";
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Account",
 };
 
 export default function Page() {
+  const session = use(auth());
+  const firstName = session.user.name.split(" ")[0];
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Rishi
+      Welcome, {firstName || "User"}
     </h2>
   );
 }
