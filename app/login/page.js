b9871fdd0318a8da7de7f22d961e0f50ebdd430f
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-26 16:08:00 +0530

    Create a sign-in page

---
+++ app/login/page.js
@@ -0,0 +1,20 @@
+import { use } from "react";
+import SignInButton from "../_components/SignInButton";
+
+export const metadata = {
+  title: "Login",
+};
+
+export default function Page({ searchParams }) {
+  const { redirectTo } = use(searchParams);
+
+  return (
+    <div className="flex flex-col gap-10 mt-10 items-center">
+      <h2 className="text-3xl font-semibold">
+        Sign in to access your guest area
+      </h2>
+
+      <SignInButton redirectTo={redirectTo} />
+    </div>
+  );
+}
