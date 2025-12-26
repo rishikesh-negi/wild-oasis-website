7751fe6c32fb517d5abac9664147c7aeec7854d2
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-26 16:19:08 +0530

    Pass cabinId into LoginMessage to create a post-login revisit path that user can be sent back to after login

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -22,7 +22,7 @@ async function Reservation({ cabin }) {
       {session?.user ? (
         <ReservationForm cabin={cabin} user={session.user} />
       ) : (
-        <LoginMessage />
+        <LoginMessage cabinId={cabin.id} />
       )}
     </div>
   );
