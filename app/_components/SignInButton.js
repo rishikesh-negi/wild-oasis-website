cd6ccee3fad5e1c978b1e1e7b0ad17862482aeb5
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-26 16:17:07 +0530

    Wrap component in form for sign-in server action, and send post-login revisit path to the sign-in server action

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -1,16 +1,20 @@
 import Image from "next/image";
+import { signInAction } from "../_lib/actions";
 
-function SignInButton() {
+function SignInButton({ redirectTo }) {
   return (
-    <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
-      <Image
-        src="https://authjs.dev/img/providers/google.svg"
-        alt="Google logo"
-        height="24"
-        width="24"
-      />
-      <span>Continue with Google</span>
-    </button>
+    <form action={signInAction.bind(this, redirectTo)}>
+      <button className="flex items-center gap-6 text-lg cursor-pointer hover:bg-primary-800 hover:text-accent-400 hover:[&>img]:scale-120 transition-all duration-150 border border-primary-800 px-8 py-4 font-medium">
+        <Image
+          src="https://authjs.dev/img/providers/google.svg"
+          className="rounded-full antialiased"
+          alt="Google logo"
+          height="24"
+          width="24"
+        />
+        <span>Continue with Google</span>
+      </button>
+    </form>
   );
 }
 
