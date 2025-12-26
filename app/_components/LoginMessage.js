b751f1be3ba9f2bf6dea1c65e35a8fb08cc015a5
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-26 16:15:11 +0530

    Send the path that redirected the user to the login page for revisit after login

--- app/_components/LoginMessage.js
+++ app/_components/LoginMessage.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
-function LoginMessage() {
+function LoginMessage({ cabinId }) {
   return (
     <div className="grid bg-primary-800 ">
       <p className="text-center text-xl py-12 self-center">
         Please{" "}
-        <Link href="/login" className="underline text-accent-500">
+        <Link
+          href={`/login?redirectTo=${encodeURIComponent(`/cabins/${cabinId}`)}`}
+          className="underline text-accent-500">
           login
         </Link>{" "}
         to reserve this
