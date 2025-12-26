800afe321477f697f65215916756da7ff0b77006
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-26 16:21:21 +0530

    Add the 'Guest area' nav link back, to allow unauthenticated users to be redirected to the login page

--- app/_components/UserProfileNavLink.js
+++ app/_components/UserProfileNavLink.js
@@ -6,10 +6,7 @@ import { auth } from "../_lib/auth";
 function UserProfileNavLink() {
   const session = use(auth());
 
-  // If there's no session, return null for now and return a "login/signup" button later:
-  if (!session) return null;
-
-  return (
+  return session?.user ? (
     <Link
       href="/account"
       className="hover:text-accent-400 transition-colors flex items-center gap-3">
@@ -21,6 +18,10 @@ function UserProfileNavLink() {
         alt="User's avatar"
         referrerPolicy="no-referrer"
       />
+      <span>Guest area</span>
+    </Link>
+  ) : (
+    <Link href="/account" className="hover:text-accent-400 transition-colors">
       <span>Guest area</span>
     </Link>
   );
