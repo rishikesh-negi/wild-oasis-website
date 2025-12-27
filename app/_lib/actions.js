3b4a3998b1726dea58459ae1eb3e6fba7c78e1ed
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-27 15:59:13 +0530

    Start implementing the server action to update guest profile

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,12 +3,16 @@
 import { signIn, signOut } from "./auth";
 
 export async function signInAction(redirectTo) {
-  const redirectionPath = redirectTo ?? "/account";
-  await signIn("google", { redirectTo: redirectionPath });
+  const targetPath = redirectTo ?? "/account";
+  await signIn("google", { redirectTo: targetPath });
 }
 
 export async function signOutAction() {
   await signOut({
     redirectTo: "/",
   });
+}
+
+export async function updateProfile() {
+  console.log("Update profile server action");
 }
