a2b854f929b5cd48613774f6b32ee4e4a8472627
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-27 15:50:48 +0530

    Add 'id' to Reservation component to scroll to it after post-authentication redirect

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -13,7 +13,9 @@ async function Reservation({ cabin }) {
   const session = await auth();
 
   return (
-    <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+    <div
+      className="grid grid-cols-2 border border-primary-800 min-h-[400px]"
+      id="reservation-form">
       <DateSelector
         settings={settings}
         bookedDates={bookedDates}
