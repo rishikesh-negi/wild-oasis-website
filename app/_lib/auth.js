ce323bbd5684aa32137eee8742fded814c559265
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-27 15:56:41 +0530

    Add NextAuth callbacks for sign-in, adding user ID to the token, and returning an updated session with the user ID

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -12,6 +13,35 @@ const authConfig = {
     // Callback to check authenticated user. Returns boolean. Enforces route protection:
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+
+    // Runs before the actual sign-in process (similar to a middleware). Gets user, account, and profile data passed into it:
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
+
+    // Make session persistent and avoid requesting guestId from DB on every session check (for the below "session" callback):
+    async jwt({ token, user }) {
+      if (user) {
+        const guest = await getGuest(user.email);
+        token.guestId = guest.id;
+      }
+
+      return token;
+    },
+
+    // The session callback to fetch user ID and provide it to the entire app. Gets access to the current session and user. Runs after the signIn callback and after each session checkout (example: the auth() function call):
+    async session({ session, token }) {
+      session.user.guestId = token.guestId;
+      return session;
     },
   },
   pages: {
