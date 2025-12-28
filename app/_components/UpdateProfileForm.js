583a75e85f052058331f07a2f351f60383749856
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-28 15:16:50 +0530

    Add 'name' attribute to input elements to access form data in server action and conditionally render the country flag

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -17,6 +17,7 @@ function UpdateProfileForm({ children, guest }) {
         <label>Full name</label>
         <input
           disabled
+          name="fullName"
           defaultValue={fullName}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
@@ -26,6 +27,7 @@ function UpdateProfileForm({ children, guest }) {
         <label>Email address</label>
         <input
           disabled
+          name="email"
           defaultValue={email}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
@@ -34,13 +36,15 @@ function UpdateProfileForm({ children, guest }) {
       <div className="space-y-2">
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
-          <Image
-            src={`/${countryFlag}`}
-            alt="Country flag"
-            width={10}
-            height={10}
-            className="h-5 rounded-sm"
-          />
+          {countryFlag && (
+            <Image
+              src={`${countryFlag}`}
+              alt="Country flag"
+              width={24}
+              height={24}
+              className="h-5 rounded-sm"
+            />
+          )}
         </div>
 
         <div className="space-y-2">{children}</div>
