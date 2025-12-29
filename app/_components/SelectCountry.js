7928baa101c989c5812118f817ac4fcb3174075c
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-29 16:07:19 +0530

    Make the component non-async and add 'key' to the select input to make it ditch the old RSC payload and display fresh data after updation through the server action

--- app/_components/SelectCountry.js
+++ app/_components/SelectCountry.js
@@ -1,11 +1,10 @@
 import { getCountries } from "@/app/_lib/data-service";
+import { use } from "react";
 
 // Let's imagine your colleague already built this component 😃
 
-async function SelectCountry({ defaultCountry, name, id, className }) {
-  "use cache";
-
-  const countries = await getCountries();
+function SelectCountry({ defaultCountry, name, id, className }) {
+  const countries = use(getCountries());
   const flag =
     countries.find((country) => country.name === defaultCountry)?.flag ?? "";
 
@@ -15,7 +14,8 @@ async function SelectCountry({ defaultCountry, name, id, className }) {
       id={id}
       // Here we use a trick to encode BOTH the country name and the flag into the value. Then we split them up again later in the server action
       defaultValue={`${defaultCountry}%${flag}`}
-      className={className}>
+      className={className}
+      key={defaultCountry}>
       <option value="">Select country...</option>
       {countries.map((c) => (
         <option key={c.name} value={`${c.name}%${c.flag}`}>
