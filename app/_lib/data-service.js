09110a31e059b8f5c568578fd7d358dbea237730
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-01 17:36:50 +0530

    Comment out reference functions. Add 'status' to the fetched Bookings fields. Remove obsolete import.

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,7 +1,6 @@
 import { eachDayOfInterval } from "date-fns";
-import { supabase } from "./supabase";
-import { cache } from "react";
 import { notFound } from "next/navigation";
+import { supabase } from "./supabase";
 
 /////////////
 // GET
@@ -84,7 +83,7 @@ export async function getBookings(guestId) {
     .from("bookings")
     // We actually also need data on the cabins as well. But let's ONLY take the data that we actually need, in order to reduce downloaded data.
     .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, totalPrice, guestId, cabinId, cabins(name, image)"
+      "id, created_at, startDate, endDate, numNights, numGuests, totalPrice, guestId, cabinId, status, cabins(name, image)"
     )
     .eq("guestId", guestId)
     .order("startDate");
@@ -182,7 +181,7 @@ export async function createBooking(newBooking) {
 
 /////////////
 // UPDATE
-
+/*
 // The updatedFields is an object which should ONLY contain the updated data
 export async function updateGuest(id, updatedFields) {
   const { data, error } = await supabase
@@ -226,3 +225,4 @@ export async function deleteBooking(id) {
   }
   return data;
 }
+*/
