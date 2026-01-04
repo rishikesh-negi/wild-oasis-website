34cb7f76b6472ced4aad073247c74253770baa27
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-04 19:52:33 +0530

    Create a component to display user's reservations in the profile's 'Reservation' section

---
+++ app/_components/ReservationsList.js
@@ -0,0 +1,34 @@
+"use client";
+
+import { useOptimistic } from "react";
+import ReservationCard from "./ReservationCard";
+import { deleteReservation } from "../_lib/actions";
+
+function ReservationsList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    // The update function. Accepts the current state and the value passed into the setter function, to perform the optimistic state update:
+    function (currentBookings, bookingId) {
+      return currentBookings.filter((booking) => booking.id !== bookingId);
+    }
+  );
+
+  async function handleDelete(bookingId) {
+    optimisticDelete(bookingId); // The setter function
+    await deleteReservation(bookingId);
+  }
+
+  return (
+    <ul className="space-y-6">
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          booking={booking}
+          onDelete={handleDelete}
+          key={booking.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationsList;
